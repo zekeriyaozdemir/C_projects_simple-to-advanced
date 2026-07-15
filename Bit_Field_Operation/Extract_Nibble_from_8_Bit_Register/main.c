972de516c81abc4fd34af79db512c77e5a21ef53
Zekeriya Ozdemir <zkdem6101@gmail.com>
2026-07-15 19:51:28 +0300

    Bit Field Operation with code traning

---
+++ Bit_Field_Operation/Extract_Nibble_from_8_Bit_Register/main.c
@@ -0,0 +1,31 @@
+/*
+This code will enable to extract high-side bits or low-side bits
+on 8-bits register, or up to 32-bits.
+*/
+
+#include <stdio.h>
+#include <stdint.h>
+
+uint8_t EXTRACT_NIBBLE(uint8_t REG, uint8_t  POS)
+{
+    if(POS == 0) 
+    {
+        printf("Lower Nibble --> ");
+        return (REG &= 0x0F); 
+    }
+    else { 
+        printf("Upper Nibble --> ");
+        return (REG &= 0xF0); 
+    }
+}
+
+int main()
+{
+    uint32_t value_REG, value_POS;
+    scanf("%hhx %u", &value_REG, & value_POS);
+    uint8_t RESULT = EXTRACT_NIBBLE(value_REG, value_POS);
+    printf("%hhu", RESULT);
+
+
+    return 0;
+}
